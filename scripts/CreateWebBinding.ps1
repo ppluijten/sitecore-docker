e921c619816a1af638ab6470386581ad693f660e
Jason Palios <jason.palios.g@gmail.com>
2019-02-22 15:24:42 +0100

    Web binding creation script (#45)
    * Initial commit Sitecore 9.1
    
    * commit pre merge
    
    * tweaks to pull request
    
    * Remove XC
    
    * XP 9.1 wip
    
    * fix identity server and solr prefix name
    
    * identity works
    
    * tweaks
    
    * default value for sxa package
    
    * CreateWebBinding
    
    * backtick removal
    
    * port and protocol are variables
    
    * new line

---
+++ scripts/CreateWebBinding.ps1
@@ -0,0 +1,18 @@
+
+Param(
+    [Parameter(Mandatory=$True)]
+    $certpath,
+    $secret = "secret",
+    [Parameter(Mandatory=$True)]
+    $sitename,
+    [Parameter(Mandatory=$True)]
+    $hostname,
+    $port = "443",
+    $protocol = "https"
+)
+
+$password = ConvertTo-SecureString -String $secret -Force -AsPlainText;
+$PFXCert = Get-PfxData -FilePath $certpath -Password $password;
+New-WebBinding -Name $sitename -IPAddress * -Port $port -Protocol $protocol -HostHeader $hostname;
+$binding = Get-WebBinding -Name $sitename -Protocol $protocol;
+$binding.AddSslCertificate($PFXCert.EndEntityCertificates.ThumbPrint, 'my')
